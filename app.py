e8a25f5103b3f85004753560fe936f9f463c3a7d
James Noah Santos <trainee_11@swapoolabs.com>
2022-05-31 17:56:33 +0800

    Simplified the app by removing unneccesary load

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
-from resources.user import UserRegister, User, UserLogin, TokenRefresh
+from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from blacklist import BLACKLIST
@@ -25,50 +25,9 @@ def create_tables():
 
 jwt = JWTManager(app)  
 
-@jwt.user_claims_loader
-def add_claims_to_jwt(identity):
-    if identity == 1:   # Instead of hard-coding, you should read from a config file or database
-        return {'is_admin': True}
-    return {'is_admin': False}
-
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
-    return decrypted_token['identity'] in BLACKLIST
-
-@jwt.expired_token_loader
-def expired_token_callback():
-    return jsonify({
-                    'description': 'token has expired',
-                	'error': 'token expired' 
-            }), 401
-
-@jwt.invalid_token_loader
-def invalid_token_callback(error):
-    return jsonify({
-                    'description': 'Signature verification failed',
-                	'error': 'invalid_token' 
-            }), 401
-
-@jwt.unauthorized_loader
-def missing_token_callback(error):
-    return jsonify({
-                    'description': 'Request does not contain access token',
-                	'error': 'authorization_required' 
-            }), 401
-
-@jwt.needs_fresh_token_loader
-def token_not_fresh_callback(error):
-    return jsonify({
-                    'description': 'The token is not fresh',
-                	'error': 'fresh_token_required' 
-            }), 401
-
-@jwt.revoked_token_loader
-def revoked_token_callback(error):
-    return jsonify({
-                    'description': 'The token has been revoked',
-                	'error': 'token_revoked' 
-            }), 401
+    return decrypted_token['jti'] in BLACKLIST
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
@@ -77,6 +36,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
 api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
