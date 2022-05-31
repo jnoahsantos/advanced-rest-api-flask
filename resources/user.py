5d46461ed2856da658d5a1d5e5797cd53a2b71e9
James Noah Santos <trainee_11@swapoolabs.com>
2022-05-31 11:51:34 +0800

    Code Cleanup

--- resources/user.py
+++ resources/user.py
@@ -21,7 +21,7 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
 
-        user = UserModel(data['username'], data['password'])
+        user = UserModel(**data)
         user.save_to_db()
 
         return {"message": "User created successfully."}, 201
