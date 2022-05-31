62f733b1c78e7c4489d5901f93f1481f3839151d
James Noah Santos <trainee_11@swapoolabs.com>
2022-06-01 07:03:58 +0800

    black formatter test

--- models/item.py
+++ models/item.py
@@ -3,33 +3,34 @@ from db import db
 
 ItemJSON = Dict[str, Union[int, str, float]]
 
+
 class ItemModel(db.Model):
-    __tablename__ = 'items'
+    __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
     price = db.Column(db.Float(precision=2))
 
-    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
-    store = db.relationship('StoreModel')
+    store_id = db.Column(db.Integer, db.ForeignKey("stores.id"))
+    store = db.relationship("StoreModel")
 
-    def __init__(self, name: str, price: float , store_id: int):
+    def __init__(self, name: str, price: float, store_id: int):
         self.name = name
         self.price = price
         self.store_id = store_id
 
     def json(self) -> ItemJSON:
         return {
-            'id': self.id,
-            'name': self.name,
-            'price': self.price,
-            'store_id': self.store_id
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "store_id": self.store_id,
         }
 
     @classmethod
     def find_by_name(cls, name: str) -> "ItemModel":
         return cls.query.filter_by(name=name).first()
-    
+
     @classmethod
     def find_all(cls) -> List["ItemModel"]:
         return cls.query.all()
--- models/store.py
+++ models/store.py
@@ -4,28 +4,30 @@ from typing import Dict, List, Union
 from models.item import ItemJSON
 
 storeJSON = Dict[str, Union[int, str, List[ItemJSON]]]
+
+
 class StoreModel(db.Model):
-    __tablename__ = 'stores'
+    __tablename__ = "stores"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80),unique=True)
+    name = db.Column(db.String(80), unique=True)
 
-    items = db.relationship('ItemModel', lazy='dynamic')
+    items = db.relationship("ItemModel", lazy="dynamic")
 
     def __init__(self, name: str):
         self.name = name
 
     def json(self) -> storeJSON:
         return {
-            'id': self.id,
-            'name': self.name,
-            'items': [item.json() for item in self.items.all()]
+            "id": self.id,
+            "name": self.name,
+            "items": [item.json() for item in self.items.all()],
         }
 
     @classmethod
     def find_by_name(cls, name: str) -> "StoreModel":
         return cls.query.filter_by(name=name).first()
-    
+
     @classmethod
     def find_all(cls) -> List["StoreModel"]:
         return cls.query.all()
