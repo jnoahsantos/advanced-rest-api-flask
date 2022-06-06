e27f9f318a157d905bcd416c69b6fca7ffe56ec4
James Noah Santos <trainee_11@swapoolabs.com>
2022-06-06 14:34:20 +0800

    Error handling for Mailgun

--- libs/mailgun.py
+++ libs/mailgun.py
@@ -2,6 +2,15 @@ import os
 from typing import List
 from requests import Response, post
 
+FAILED_LOAD_API_KEY = "Failed to load MailGun API key."
+FAILED_LOAD_DOMAIN = "Failed to load MailGun domain."
+ERROR_SENDING_EMAIL = "Error in sending confirmation email, user registration failed."
+
+
+class MailGunException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
 
 class Mailgun:
     MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY", None)
@@ -14,8 +23,13 @@ class Mailgun:
     def send_email(
         cls, email: List[str], subject: str, text: str, html: str
     ) -> Response:
+        if cls.MAILGUN_API_KEY is None:
+            raise MailGunException(FAILED_LOAD_API_KEY)
 
-        return post(
+        if cls.MAILGUN_DOMAIN is None:
+            raise MailGunException(FAILED_LOAD_DOMAIN)
+
+        response = post(
             f"https://api.mailgun.net/v3/{cls.MAILGUN_DOMAIN}/messages",
             auth=("api", cls.MAILGUN_API_KEY),
             data={
@@ -26,3 +40,8 @@ class Mailgun:
                 "html": html,
             },
         )
+
+        if response.status_code != 200:
+            raise MailGunException(ERROR_SENDING_EMAIL)
+
+        return response
