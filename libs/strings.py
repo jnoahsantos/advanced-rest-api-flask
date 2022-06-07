ec7e3a23fd92b68218346dc4d74def8c2377cb18
James Noah Santos <trainee_11@swapoolabs.com>
2022-06-07 18:24:33 +0800

    Adding new languange

--- libs/strings.py
+++ libs/strings.py
@@ -7,7 +7,7 @@ If language changes, set `libs.strings.default_locale` and run `libs.strings.ref
 """
 import json
 
-default_locale = "en-gb"
+default_locale = "es-es"
 cached_strings = {}
 
 
--- schemas/store.py
+++ schemas/store.py
@@ -1,4 +1,4 @@
-from ma import ma 
+from ma import ma
 from models.store import StoreModel
 from models.item import ItemModel
 from schemas.item import ItemSchema
@@ -6,10 +6,9 @@ from schemas.item import ItemSchema
 
 class StoreSchema(ma.SQLAlchemyAutoSchema):
     items = ma.Nested(ItemSchema, many=True)
+
     class Meta:
         model = StoreModel
-        load_only = ("store",)
-        dump_only = ("id")
-        load_istance = True
+        load_instance = True
+        dump_only = ("id",)
         include_fk = True
-
