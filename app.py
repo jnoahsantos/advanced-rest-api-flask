10c7953624554a4aa48eb0790ddf25d03bf14140
James Noah Santos <trainee_11@swapoolabs.com>
2022-06-08 23:58:08 +0800

    Starter Code for ThirdParty login with OAuth github

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-import os
-
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from flask_migrate import Migrate
 from marshmallow import ValidationError
+from dotenv import load_dotenv
 
 from db import db
 from ma import ma
@@ -12,16 +10,11 @@ from resources.user import UserRegister, UserLogin, User
 
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DATABASE_URI", "sqlite:///data.db"
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.secret_key = "jose"
+load_dotenv(".env")
+app.config.from_object("default_config")
+app.config.from_envvar("APPLICATION_SETTINGS")
 api = Api(app)
 jwt = JWTManager(app)
-migrate = Migrate(app, db)
 
 
 @app.before_first_request
@@ -41,4 +34,4 @@ api.add_resource(UserLogin, "/login")
 if __name__ == "__main__":
     db.init_app(app)
     ma.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run(port=5000)
--- models/user.py
+++ models/user.py
@@ -7,7 +7,6 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(80), nullable=False, unique=True)
 
     @classmethod
     def find_by_username(cls, username: str) -> "UserModel":
